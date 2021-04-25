31a233383fcc74b7045debe1cf62233c44ab5907
Shinichi Ishimura <sishimura@seqsense.com>
2021-04-25 19:26:04 +0900

    Enable dummy S3 backend

--- test/backend.tf
+++ test/backend.tf
@@ -1,7 +1,7 @@
-//terraform {
-//  backend "s3" {
-//    bucket = "my-terraform-state-bucket"
-//    key    = "terraform.tfstate"
-//    region = "ap-northeast-1"
-//  }
-//}
+terraform {
+  backend "s3" {
+    bucket = "my-terraform-state-bucket"
+    key    = "terraform.tfstate"
+    region = "ap-northeast-1"
+  }
+}
